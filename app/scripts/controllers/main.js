30db431fd23e1aa162011ff89b954f4b559c2091
Christoffer Ekeroth <christoffer@campanja.com>
2013-09-12 10:30:20 +0200

    Rewrite test application

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,33 @@
-'use strict';
+(function () {
+  'use strict';
 
-angular.module('ngE2e2App')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  var controller = function($scope, $http) {
+    $scope.playlists = [{
+      name: 'Popular',
+      url: '/fixtures/popular.json'
+    },
+    {
+      name: 'JohnyTex',
+      url: '/fixtures/johnytex.json'
+    }];
+
+    $scope.setPlaylist = function(playlist) {
+      $scope.selectedPlaylist = playlist;
+    };
+
+    $scope.$watch('selectedPlaylist', function(playlist) {
+      playlist && fetchSongs(playlist);
+    });
+
+    function fetchSongs(playlist) {
+      $http.get(playlist.url)
+        .then(function(response) {
+          delete response.data.version;
+          $scope.songs = response.data
+        });
+    }
+  };
+
+  angular.module('ngE2eApp')
+    .controller('MainCtrl', ['$scope', '$http', controller]);
+})();
